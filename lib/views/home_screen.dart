32f0a120c5bb8c041061ee9db7f2953cd00f8ccd
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-07-05 20:01:34 +0500

    updated

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -5,6 +5,10 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CipherMoji"),
+      ),
+    );
   }
 }
