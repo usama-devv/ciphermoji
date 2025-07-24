0fbd0ad5b8944ba93bf7dcc0ccc68356f85f9727
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-07-24 11:13:41 +0500

    animated theme switch

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -24,12 +24,43 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true,
         elevation: 4,
         actions: [
-          Obx(()=>Switch(value: controller.isDark.value, onChanged: (value){
-            controller.toggleTheme();
-          }),),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: GestureDetector(
+              onTap: controller.toggleTheme,
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: 50,
+                height: 30,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: controller.isDark.value
+                      ? Colors.grey[800]
+                      : Colors.yellow[600],
+                ),
+                child: Align(
+                  alignment: controller.isDark.value
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6),
+                    child: Icon(
+                      controller.isDark.value
+                          ? Icons.nightlight_round
+                          : Icons.wb_sunny,
+                      color: controller.isDark.value
+                          ? Colors.white
+                          : Colors.orange,
+                      size: 18,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
-      
+
     );
   }
 }
