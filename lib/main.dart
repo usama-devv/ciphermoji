4d78dfafaac1a0a7634c3032104dfb2266cafd3d
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-08-19 20:23:26 +0500

    added hive

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:ciphermoji/view_model/controller/theme_controller.dart';
 import 'package:ciphermoji/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive/hive.dart';
+// import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
